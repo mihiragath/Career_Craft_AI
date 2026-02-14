8b3356514c916bc1ddeb21321ee5b087c320c1c5
mihiragath <mihiragath@users.noreply.github.com>
2026-02-14 14:32:33 +0530

    fix: remove external URL for Watch Demo link

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,11 +46,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-<<<<<<< HEAD
           <Link href="https://www.youtube.com/roadsidecoder">
-=======
-          <Link href="">
->>>>>>> master
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
